c6975e76f4e099f48d7a8d4d72d9322fb3060db8
EC2 Default User <ec2-user@ip-172-26-5-87.ap-northeast-1.compute.internal>
2022-12-12 05:44:01 +0000

    modify input layout

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /**
- * @file mofron-comp-mcarousel/index.js
- * @brief mobile carousel component for mofron
+ * @file mofron-comp-codeinput/index.js
+ * @brief code input component for mofron
  * @license MIT
  */
-const Text    = require('mofron-comp-text');
-const Input   = require('mofron-comp-input');
-const Line    = require('mofron-comp-line');
-const comutl  = mofron.util.common;
-const ConfArg = mofron.class.ConfArg;
+const Input     = require('mofron-comp-input');
+const loMargin  = require('mofron-layout-margin');
+const Key       = require('mofron-event-key');
+const ConfArg   = mofron.class.ConfArg;
+const comutl    = mofron.util.common;
 
 module.exports = class extends mofron.class.Component {
     /**
@@ -15,17 +15,19 @@ module.exports = class extends mofron.class.Component {
      * 
      * @param (mixed) 
      *                key-value: component config
+     * @short 
      * @type private
      */
     constructor (p1) {
         try {
             super();
             this.modname("CodeInput");
-            this.shortForm('digits');
+	    this.shortForm("digits");
             
 	    /* init config */
             this.confmng().add('digits', { type:'number', init:0 });
-            this.confmng().add('left', { type:'size', init:'0.1rem' });
+            this.confmng().add('mainColor', { type:'color' });
+	    this.confmng().add('codeEvent', { type:'event', list:true })
             
 	    if (0 < arguments.length) {
                 this.config(p1);
@@ -44,47 +46,107 @@ module.exports = class extends mofron.class.Component {
     initDomConts () {
         try {
             super.initDomConts();
-            let inp_area = new mofron.class.Component({
-                               style: { 'display':'flex' }
-                           });
-            this.child([inp_area, this.keyboard()]);
+	    this.style({
+                'display': 'flex'
+	    });
+            this.digits(6);
+            
+            //let txt_area = new mofron.class.Component();
+	    //this.child(txt_area);
+
+	    this.layout([
+                new loMargin('left', '0.2rem')
+	    ]);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
-    
-    keyboard (prm) {
+
+    text (prm) {
         try {
-	    if (undefined !== prm) {
-                prm.config({
-                   size:  new ConfArg('100%','40%'),
-		   style: {
-		       'position':  'fixed',
-		       'bottom':    '0rem'
-		   },
-		   //child: 
-		   baseColor: [240,240,240],
-		   visible: false
-		});
+            
+	} catch (e) {                        
+            console.error(e.stack);
+            throw e; 
+        }
+    }
+
+    codeEvent (fnc,prm) {
+        try {
+            return this.confmng(
+                       'codeEvent',
+                       (undefined === fnc) ? undefined : [fnc,prm]
+                   );
+	} catch (e) {             
+            console.error(e.stack);
+            throw e;          
+        } 
+    }
+    
+    keyEvent (p1,p2,p3) {
+        try {
+	    if ( ('Backspace' === p2) ||
+	         ('Enter' === p2) ||
+		 (0 === p2.indexOf('Arrow')) ) {
+                return;
 	    }
-            return this.innerComp('keyboard', prm, mofron.class.Component);
+	    let inp_lst   = p3.getInput();
+	    let focus_idx = 0;
+            let code_lst  = [];
+	    for (let idx in inp_lst) {
+                if (inp_lst[idx].id() === p1.id()) {
+                    focus_idx = parseInt(idx);
+		}
+                code_lst.push(inp_lst[idx].value());
+	    }
+	    if (focus_idx < (inp_lst.length-1)) {
+                inp_lst[focus_idx+1].focus(true);
+		return;
+	    }
+            let code_evt = p3.codeEvent();
+            for (let cidx in code_evt) {
+                code_evt[cidx][0](p3, code_lst, code_evt[cidx][1]);
+            }
 	} catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
 
-    beforeRender () {
-        try {
-	    let inp = new mofron.class.Component({
-	                  size:  new ConfArg('0.3rem', '0.6rem'),
-                          child: [ new Text(), new Line('100%') ],
-			  style: { 'margin-left': this.left() }
-	              });
+    value () {
+        
+    }
 
-            for (let idx=0;idx < this.digits(); idx++) {
-                this.child()[0].child(inp);
+    reset () {
+        try {
+            let inp = this.getInput();
+            for (let idx in inp) {
+                inp[idx].value("");
+            }
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+	}
+    }
+
+    afterRender () {
+        try {
+            super.afterRender();
+            let inp_cnt = this.digits();
+            for (let inp_idx=0;inp_idx < inp_cnt;inp_idx++) {
+	        let set_inp = new Input({
+                                  width:  '0.4rem', height: '0.6rem',
+                                  style:  { 'text-align':'center' },
+				  event: new Key({
+				             listener: new ConfArg(this.keyEvent,this),
+					     type: 'keyup'
+					 })
+                              });
+                this.child(set_inp);
+                if (null !== this.mainColor()) {
+                    set_inp.accentColor(this.mainColor());
+                }
             }
         } catch (e) {
             console.error(e.stack);
@@ -92,27 +154,67 @@ module.exports = class extends mofron.class.Component {
         }
     }
 
-    left (prm) {
+    getInput (prm) {
         try {
-            return this.confmng('left',prm);
+            let chd = this.child();
+            let ret     = [];
+            for (let cidx in chd) {
+                if (true === comutl.isinc(chd[cidx],'Input')){
+                    ret.push(chd[cidx]);
+		}
+            }
+	    return ret;
 	} catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
-    
+
     digits (prm) {
         try {
-            return this.confmng('digits',prm);
-        } catch (e) {
+            return this.confmng("digits", prm);
+	} catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
 
+    marginLeft (prm) {
+        try {
+            return this.layout({ modname:'Margin' }).value(prm);
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+	}
+    }
+
     mainColor (prm, opt) {
         try {
-            return this.keyboard().baseColor(prm,opt);
+	    return this.confmng("mainColor", prm);
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+
+    width (prm,opt) {
+        try {
+            let inp_lst = this.getInput();
+	    for (let idx in inp_lst) {
+                inp_lst[idx].width(prm,opt);
+	    }
+	} catch (e) {
+            console.error(e.stack);
+	    throw e;
+	}
+    }
+
+    height (prm,opt) {
+        try {
+            let inp_lst = this.getInput();
+            for (let idx in inp_lst) {
+                inp_lst[idx].height(prm,opt);
+            }
 	} catch (e) {
             console.error(e.stack);
             throw e;
